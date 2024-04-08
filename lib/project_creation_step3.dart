57b3f88245775fc1b0de06b211ba55a5b6ba11d9
Kor-smg569 <smg56928@gmail.com>
2024-04-09 03:56:20 +0900

    2024-04-09 step3 '길게 누르기' 시 선이 생성되는 기능 추가

--- lib/project_creation_step3.dart
+++ lib/project_creation_step3.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:ui' as ui;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'project_creation_step4.dart';
 import 'project_data.dart';
@@ -25,6 +26,7 @@ class _ProjectCreationStep3State extends State<ProjectCreationStep3> {
   List<List<Offset>> _lines = [];
   bool _isTouchEnabled = false;
   GlobalKey _imageKey = GlobalKey();
+  Offset? _temporaryPoint; // Temporary point for dynamic line drawing
 
   void _toggleTouch() {
     setState(() {
@@ -32,24 +34,57 @@ class _ProjectCreationStep3State extends State<ProjectCreationStep3> {
     });
   }
 
-  void _addPoint(Offset point) {
-    if (!_isTouchEnabled) return;
-
+  Offset _convertToRelativePosition(Offset globalPosition) {
     final RenderBox imageBox = _imageKey.currentContext!.findRenderObject() as RenderBox;
-    final Offset imagePosition = imageBox.localToGlobal(Offset.zero); // 이미지의 글로벌 위치를 가져옵니다.
-    final Size imageSize = imageBox.size; // 이미지 위젯의 크기를 가져옵니다.
+    final Offset imagePosition = imageBox.localToGlobal(Offset.zero);
+    final Size imageSize = imageBox.size;
 
-    // 이미지 내에서의 상대적 위치를 계산합니다.
-    final Offset relativePosition = point - imagePosition;
+    final Offset relativePosition = globalPosition - imagePosition;
     final double scaleX = relativePosition.dx / imageSize.width;
     final double scaleY = relativePosition.dy / imageSize.height;
 
+    return Offset(scaleX, scaleY);
+  }
+
+  void _handleLongPressStart(LongPressStartDetails details) {
+    if (!_isTouchEnabled) return;
+
+    final Offset relativePosition = _convertToRelativePosition(details.globalPosition);
     setState(() {
-      _points.add(Offset(scaleX, scaleY));
+      _points.add(relativePosition);
+    });
+  }
+
+  void _handleLongPressMoveUpdate(LongPressMoveUpdateDetails details) {
+    if (!_isTouchEnabled) return;
+
+    final Offset relativePosition = _convertToRelativePosition(details.globalPosition);
+    setState(() {
+      _temporaryPoint = relativePosition;
+    });
+  }
+
+  void _handleLongPressEnd(LongPressEndDetails details) {
+    if (!_isTouchEnabled || _temporaryPoint == null) return;
+
+    setState(() {
+      _points.add(_temporaryPoint!);
       if (_points.length == 2) {
         _lines.add(List.from(_points));
         _points.clear();
       }
+      _temporaryPoint = null;
+    });
+  }
+
+  void _undoLastAction() {
+    setState(() {
+      if (_points.isNotEmpty) {
+        _points.removeLast();
+      } else if (_lines.isNotEmpty) {
+        _lines.removeLast();
+      }
+      _temporaryPoint = null;
     });
   }
 
@@ -70,32 +105,41 @@ class _ProjectCreationStep3State extends State<ProjectCreationStep3> {
         ],
       ),
       body: GestureDetector(
-        onTapDown: (TapDownDetails details) {
-          _addPoint(details.globalPosition);
-        },
+        onLongPressStart: _handleLongPressStart,
+        onLongPressMoveUpdate: _handleLongPressMoveUpdate,
+        onLongPressEnd: _handleLongPressEnd,
         child: Stack(
           children: [
-            Center(
+            Container(
               child: Image.file(
                 File(widget.imagePath),
-                key: _imageKey, // 이미지 위젯에 GlobalKey 할당
+                key: _imageKey,
                 fit: BoxFit.contain,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 119.4),
+            Positioned.fill(
               child: CustomPaint(
-                painter: LinePainter(lines: _lines, imageKey: _imageKey),
-                size: Size.infinite,
+                painter: LinePainter(lines: _lines, points: _points, imageKey: _imageKey, temporaryPoint: _temporaryPoint),
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _toggleTouch,
-        tooltip: 'Toggle Touch',
-        child: Icon(_isTouchEnabled ? Icons.not_interested : Icons.touch_app),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _toggleTouch,
+            tooltip: 'Toggle Touch',
+            child: Icon(_isTouchEnabled ? Icons.not_interested : Icons.touch_app),
+          ),
+          SizedBox(height: 20),
+          FloatingActionButton(
+            onPressed: _undoLastAction,
+            tooltip: 'Undo Last Action',
+            child: Icon(Icons.undo),
+          ),
+        ],
       ),
     );
   }
@@ -103,9 +147,11 @@ class _ProjectCreationStep3State extends State<ProjectCreationStep3> {
 
 class LinePainter extends CustomPainter {
   final List<List<Offset>> lines;
+  final List<Offset> points;
   final GlobalKey imageKey;
+  final Offset? temporaryPoint;
 
-  LinePainter({required this.lines, required this.imageKey});
+  LinePainter({required this.lines, required this.points, required this.imageKey, this.temporaryPoint});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -115,21 +161,25 @@ class LinePainter extends CustomPainter {
       ..strokeCap = ui.StrokeCap.round;
 
     final RenderBox imageBox = imageKey.currentContext!.findRenderObject() as RenderBox;
-    final Size imageSize = imageBox.size; // 이미지 위젯의 실제 크기를 가져옵니다.
+    final Size imageSize = imageBox.size;
 
     for (var line in lines) {
-      for (int i = 0; i < line.length; i++) {
-        // 실제 이미지 크기에 맞게 포인트 위치를 조정합니다.
-        final Offset scaledPoint = Offset(line[i].dx * imageSize.width, line[i].dy * imageSize.height);
-        canvas.drawCircle(scaledPoint, 3.0, paint);
-        if (i < line.length - 1) {
-          final Offset nextPoint = Offset(line[i + 1].dx * imageSize.width, line[i + 1].dy * imageSize.height);
-          canvas.drawLine(scaledPoint, nextPoint, paint);
-        }
-
-        //  * imageSize.width, line[i].dy * imageSize.height
-        //  * imageSize.width, line[i + 1].dy * imageSize.height
+      for (int i = 0; i < line.length - 1; i++) {
+        final Offset startPoint = Offset(line[i].dx * imageSize.width, line[i].dy * imageSize.height);
+        final Offset endPoint = Offset(line[i + 1].dx * imageSize.width, line[i + 1].dy * imageSize.height);
+        canvas.drawLine(startPoint, endPoint, paint);
+        canvas.drawCircle(startPoint, 2.0, paint); // Draw start point
+        canvas.drawCircle(endPoint, 2.0, paint); // Draw end point
       }
+    }
+
+    // Draw temporary line and point if applicable
+    if (points.isNotEmpty && temporaryPoint != null) {
+      final Offset startPoint = Offset(points.last.dx * imageSize.width, points.last.dy * imageSize.height);
+      final Offset endPoint = Offset(temporaryPoint!.dx * imageSize.width, temporaryPoint!.dy * imageSize.height);
+      canvas.drawLine(startPoint, endPoint, paint);
+      canvas.drawCircle(startPoint, 2.0, paint); // Draw start point
+      canvas.drawCircle(endPoint, 2.0, paint); // Temporary end point
     }
   }
 
